55c436e648989f707d92917c0aea214a35437656
poojachavan1420 <chavanpooja92241@gmail.com>
2024-10-22 14:05:14 +0530

    changes

---
+++ 3 React/7-using-bootstrap/src/App.jsx
@@ -0,0 +1,19 @@
+
+function App() {
+  
+  return (
+    <>
+    <div className="m-3">
+     <ul className="list-group list-group-numberedr">
+     <li className="list-group-item">Home</li>
+     <li className="list-group-item active">contact</li>
+     <li className="list-group-item disabled">products</li>
+
+     </ul>
+     </div>
+
+    </>
+  )
+}
+
+export default App
