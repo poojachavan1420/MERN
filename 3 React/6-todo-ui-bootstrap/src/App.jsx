956dc585a095b120648885026eda4aee89d31db5
poojachavan1420 <chavanpooja92241@gmail.com>
2024-10-29 14:49:36 +0530

    changes

--- 3 React/6-todo-ui-bootstrap/src/App.jsx
+++ 3 React/6-todo-ui-bootstrap/src/App.jsx
@@ -1,20 +1,18 @@
+
 import AppName from "./components/AppName";
 import AddTodo from "./components/AddTodo";
 import TodoItems from "./components/TodoItems";
 import Button from "./components/Button";
 
 function App() {
-const todoItems =[{id:1,todoText:'Buy Milk', todoDate:'4-sept-2024'},
-                  {id:2,todoText:'Go to college', todoDate:'Weekday'},
-                  {id:3,todoText:'Exercise', todoDate:'Everyday'}];
   return(
     <>
       <center>
        <AppName/>
        <AddTodo/>
        <Button/>
-       <TodoItems todoItems={todoItems}/>
-      </center>
+       <TodoItems/>
+       </center>
     </>
   );
 };
