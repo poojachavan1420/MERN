90081f06c29b9db9b69ebaa0967628943357b224
poojachavan1420 <chavanpooja92241@gmail.com>
2025-01-09 10:47:51 +0530

    express deepdive styling

--- 5 NodeJs and ExpressJs/7 airbnb/app.js
+++ 5 NodeJs and ExpressJs/7 airbnb/app.js
@@ -1,90 +1,28 @@
+// Core Modules
+const path = require("path");
+
 // External Module
-const express = require('express');
-const fs = require('fs');
-const bodyparser = require ('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+
+// Local Module
+const hostRouter = require("./routers/hostRouter");
+const storeRouter = require("./routers/storeRouter");
+const rootDir = require('./util/path-util');
 
 const app = express();
 
-app.use(bodyparser.urlencoded({extended:true}));
-
-app.get("/",(req,res,next)=>{
-   res.send(`
-    <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hamara airbnb</title>
-</head>
-<body>
-    <h1>Welcome to airbnb</h1>
-    <a href="/add-home">Add Home</a>
-</body>
-</html>`)
-})
-
-app.post("/add-home",(req,res,next)=>{
-   console.log(req.body);
-   res.send(`
-    <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-   
-    <title>Home Added</title>
-</head>
-<body>
-    <h1>Home added Successfully</h1>
-    <a href="/">Go to home</a>
-</body>
-</html>`)
-})
-
-app.get("/add-home",(req,res,next) =>{
-    res.send(`
-        <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-  
-    <title>Add Home</title>
-</head>
-<body>
-    <h1>Add your Home</h1>
-    <form action="/add-home" method="post">
-        <input type="text" 
-        name="houseName"
-        placeholder="Name of your house">
-          <input type="submit" value="Add Home">
-    </form>
-
-</body>
-</html>
-        `)
-});
+app.use(express.static(path.join(rootDir, "public")));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(storeRouter);
+app.use("/host", hostRouter);
 
 app.use((req, res, next) => {
-  console.log('Request Received', req.url, req.method , req.body);
-  next();
+  res.statusCode = 404;
+  res.sendFile(path.join(rootDir, "views", "404.html"));
 });
 
-app.use((req, res, next) => {
-    res.statusCode = 404;
-    res.write(`
-      <!DOCTYPE html>
-        <html lang="en">
-        <head>
-          <title>Page Not Found</title>
-        </head>
-        <body>
-          <h1>404 Page Not Found</h1>
-        </body>
-        </html>
-      `);
-    res.end();
-  })
-  
-  const PORT = 5173;
-  app.listen(PORT, () => {
-    console.log(`Server running at: http://localhost:${PORT}`);
-  });
+const PORT = 5173;
+app.listen(PORT, () => {
+  console.log(`Server running at: http://localhost:${PORT}`);
+});
