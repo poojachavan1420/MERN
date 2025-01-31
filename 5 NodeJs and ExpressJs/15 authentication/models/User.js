9bfb45e90859d8d686f93af73b8198de689c78d2
poojachavan1420 <chavanpooja92241@gmail.com>
2025-01-31 19:56:23 +0530

    Request validation

---
+++ 5 NodeJs and ExpressJs/15 authentication/models/User.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+
+const homeSchema = new mongoose.Schema({
+    firstName: {type: String, required: true},
+    lastName: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+    userType: {type: String, required: true , enum: ['host','guest']},
+    
+
+});
+
+
+
+module.exports = mongoose.model("User", homeSchema);
